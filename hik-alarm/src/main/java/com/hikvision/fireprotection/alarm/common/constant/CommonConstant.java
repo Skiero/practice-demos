4b60e67f01f9b69ccbe96a87b564f1b6dca07b03
Skiero <edckb03@163.com>
2020-12-21 14:25:51 +0800

    [MOD]增加手机号码的正则表达式。

--- hik-alarm/src/main/java/com/hikvision/fireprotection/alarm/common/constant/CommonConstant.java
+++ hik-alarm/src/main/java/com/hikvision/fireprotection/alarm/common/constant/CommonConstant.java
@@ -9,4 +9,5 @@ package com.hikvision.fireprotection.alarm.common.constant;
  */
 public interface CommonConstant {
     String SUCCESS_CODE = "0";
+    String MOBILE_REGEX = "^([1][3,4,5,6,7,8,9])\\d{9}$";
 }
